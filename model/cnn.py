364ff5be217f01a0330b5f3512beddfc31a98564
Josua <josua.faller@ptb.de>
2024-08-23 10:46:21 +0200

    Make cnn for MNIST smaller

--- model/cnn.py
+++ model/cnn.py
@@ -4,7 +4,15 @@ from torch import nn, Tensor
 def conv_block(in_channels: int, out_channels: int, pool: bool=False
     ) -> nn.Module:
     layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-              nn.BatchNorm2d(out_channels, track_running_stats = False),
+              nn.ReLU(inplace=True)]
+    if pool: 
+        layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+
+def conv_block_bn(in_channels: int, out_channels: int, pool: bool=False
+    ) -> nn.Module:
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
               nn.ReLU(inplace=True)]
     if pool: 
         layers.append(nn.MaxPool2d(2))
@@ -20,11 +28,13 @@ class cnn_small(nn.Module):
         
         self.conv1 = conv_block(C, 32, pool=True)
         self.conv2 = conv_block(32, 32, pool=True)
-        self.classifier = nn.Sequential(nn.Flatten(), 
-                                        nn.Linear(1568, n_class))
-        
+        self.classifier = nn.Sequential(nn.Flatten(),
+                                        nn.Linear(288, n_class)) 
+
+    
     def forward(self, xb: Tensor) -> Tensor:
         out = self.conv1(xb)
+        out = self.conv2(out)
         out = self.conv2(out)
         out = self.classifier(out)
         return out
