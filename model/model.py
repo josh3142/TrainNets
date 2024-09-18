aec6021e87720a9c57f9d7b0487620f123b26fc0
Josua <josua.faller@ptb.de>
2024-09-18 17:26:58 +0200

    Add ResNet18

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torchvision.models import resnet18, ResNet18_Weights
 
 from model.mlp import MLP
 from model.resnet9 import ResNet9
@@ -11,10 +12,15 @@ def count_parameters(model: nn.Module):
 def get_model(name: str, **kwargs) -> nn.Module:
     if name.startswith("mlp"):
         model = MLP(**kwargs)
-    elif name == "resnet9":
+    elif name=="resnet9":
         model = ResNet9(**kwargs)
-    elif name == "cnn_small":
+    elif name=="cnn_small":
         model = cnn_small(**kwargs)
+    elif name=="resnet18":
+        pretrain = kwargs["pretrain"]
+        n_class = kwargs["n_class"]
+        model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1 if pretrain else None)
+        model.fc = nn.Linear(in_features=512, out_features=n_class, bias=True)
     else:
         raise NotImplementedError(name)
     
