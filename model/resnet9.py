539fa57b62fc04c85b5c3f64f10cd7979c4569d6
Josua <josua.faller@ptb.de>
2024-12-20 15:51:46 +0100

    Change Resnet9 architecture:
    1. track_running_stats for BatchNorm
    2. Reduce size of channels

--- model/resnet9.py
+++ model/resnet9.py
@@ -4,7 +4,7 @@ from torch import nn, Tensor
 def conv_block(in_channels: int, out_channels: int, pool: bool=False
     ) -> nn.Module:
     layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-              nn.BatchNorm2d(out_channels, track_running_stats = False),
+              nn.BatchNorm2d(out_channels, track_running_stats=True),
               nn.ReLU(inplace=True)]
     if pool: 
         layers.append(nn.MaxPool2d(2))
@@ -23,15 +23,15 @@ class ResNet9(nn.Module):
         self.res1 = nn.Sequential(conv_block(128, 128), 
                                   conv_block(128, 128))
         
-        self.conv3 = conv_block(128, 256, pool=True)
-        self.conv4 = conv_block(256, 512, pool=True)
-        self.res2 = nn.Sequential(conv_block(512, 512), 
-                                  conv_block(512, 512))
+        self.conv3 = conv_block(128, 128, pool=True)
+        self.conv4 = conv_block(128, 64, pool=True)
+        self.res2 = nn.Sequential(conv_block(64, 64), 
+                                  conv_block(64, 64))
 
         self.classifier = nn.Sequential(nn.MaxPool2d(4),
                                         nn.Flatten(), 
-                                        nn.Linear(512, n_class))
-        
+                                        nn.Linear(64, n_class))
+
     def forward(self, xb: Tensor) -> Tensor:
         out = self.conv1(xb)
         out = self.conv2(out)
